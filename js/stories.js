ef9573c005c2fc041a4c96fbbf6ee0d4d1603400
tdhood <taylordannhood@gmail.com>
2022-06-03 15:36:57 -0700

    addFavorites!!!

--- js/stories.js
+++ js/stories.js
@@ -25,6 +25,8 @@ function generateStoryMarkup(story) {
   const hostName = story.getHostName();
   return $(`
       <li id="${story.storyId}">
+      <span class="star">
+        <i class="far fa-star"></i>
         <a href="${story.url}" target="a_blank" class="story-link">
           ${story.title}
         </a>
@@ -79,3 +81,38 @@ async function submitNewStory(evt) {
 
 $('#submit-form').on('submit', submitNewStory);
 
+/**this method will add a story to a users favorites array */
+//check if story is NOT in favorites, run function (else run remove function)
+//toggle star class='filled'
+async function addFavorites(evt) {
+  console.log("evt",evt)
+  console.log('evtTarget', $(evt.target)[0])
+  const storyId = $(evt.target).closest('li').attr('id')
+  console.log('storyId', storyId)
+  if($(evt.target).hasClass("far fa-star")) {
+   const response = await axios({
+     url:`${BASE_URL}/users/${currentUser.username}/favorites/${storyId}`,
+     method: "POST",
+     data: {token: currentUser.loginToken}
+   })
+   console.log(response)
+  }
+  
+
+  // static async login(username, password) {
+  //   const response = await axios({
+  //     url: `${BASE_URL}/login`,
+  //     method: "POST",
+  //     data: { user: { username, password } },
+  //   });
+
+  $(evt.target).removeClass("far fa-star");
+  $(evt.target).addClass("fas fa-star");
+  //console.log(evt.tar)
+} 
+
+$('#all-stories-list').on('click', addFavorites)
+
+/**this method will remove a story from a users favorites array */
+//toggle star class='unfilled'
+//<i class="fas fa-star"></i> filled
